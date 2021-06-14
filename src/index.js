172d1544e0cf27f03d877a770f77f8f48cbe38e9
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-06-13 22:05:21 -0300

    feat(app): watching button for event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(17, 'in', 'both');
+
+button.watch((err, value) => {
+  if (err) {
+    console.error('button.watch error', err);
+    return;
+  }
+
+  console.info(`Button value is ${value}`);
+});
+
+process.on('SIGINT', () => {
+  button.unexport();
+});
