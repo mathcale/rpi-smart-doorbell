1076fee388e27edcb5d4faa1c977be3b16c2ef42
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-06-14 00:58:02 -0300

    feat: implementing services on main file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
+require('dotenv').config();
 const Gpio = require('onoff').Gpio;
+
+const discordService = require('./services/discordService');
+const cameraService = require('./services/cameraService');
+
 const button = new Gpio(17, 'in', 'both');
 
-button.watch((err, value) => {
+button.watch(async (err, value) => {
   if (err) {
-    console.error('button.watch error', err);
+    console.error('[ERROR] button.watch:', err);
     return;
   }
 
-  console.info(`Button value is ${value}`);
+  if (value === 0) {
+    return;
+  }
+
+  try {
+    const imageUrl = await cameraService.takePictureAndUpload();
+    const payload = discordService.buildPayload(imageUrl);
+
+    await discordService.sendMessage(payload);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 process.on('SIGINT', () => {
   button.unexport();
 });
+
+console.info('App is running!');
